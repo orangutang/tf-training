d392c00e8d4ee1f452a5d15ac2209f0f04c6049e
orangutang <23269018+orangutang@users.noreply.github.com>
2020-01-29 11:04:41 +1300

    Update interpolationSyntax_main.tf
    Substitute in some variables with defaults

--- interpolationSyntax_main.tf
+++ interpolationSyntax_main.tf
@@ -1,16 +1,31 @@
-# Commit change - 
-# Download the latest Ghost image
-resource "docker_image" "image_id" {
-  name = "ghost:latest"
+variable "container_name" {
+  description = "Name of container"
+  default = "blog"
+}
+variable "image" {
+  description = "image for container"
+  default = "ghost:latest"
+}
+variable "int_port" {
+  description = "internal port for container"
+  default = "2368"
+}
+variable "ext_port" {
+  description = "external port for container"
+  default = "80"
 }
 
+# Download the latest Ghost Image
+resource "docker_image" "image_id" {
+  name = "${var.image}"
+}
 # Start the Container
 resource "docker_container" "container_id" {
-  name  = "blog"
+  name  = "${var.container_name}"
   image = "${docker_image.image_id.latest}"
   ports {
-    internal = "2368"
-    external = "80"
+    internal = "${var.int_port}"
+    external = "${var.ext_port}"
   }
 }
 
